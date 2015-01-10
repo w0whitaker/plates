bc447a1f1dd336c137f8ff64e6d7bc9ee43bf1bf
brazilla-ray <william@mrwilliamwhitaker.com>
2015-01-10 13:57:06 -0500

    finish adding site logo to all pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,35 +1,28 @@
 require 'test_helper'
-
 class SiteLayoutTest < ActionDispatch::IntegrationTest
- 
  test "site logo on home page" do
  	get root_path
  	assert_template 'static_pages/home'
- 	assert_select "div.logo"
+ 	assert_select "a[href=?]", root_path
  end
-
  test "site logo on contact page" do
  	get contact_path
  	assert_template 'static_pages/contact'
- 	assert_select "div.logo"
+ 	assert_select "a[href=?]", root_path
  end
- 
  test "site logo on cv page" do
  	get cv_path
  	assert_template 'static_pages/cv'
- 	assert_select "div.logo"
+ 	assert_select "a[href=?]", root_path
  end
-
  test "site logo on images page" do
  	get images_path
  	assert_template 'static_pages/images'
- 	assert_select "div.logo"
+ 	assert_select "a[href=?]", root_path
  end
-
  test "site logo on maintenance page" do
  	get maintenance_path
  	assert_template 'static_pages/maintenance'
- 	assert_select "div.logo"
+ 	assert_select "a[href=?]", root_path
  end
-  
 end
