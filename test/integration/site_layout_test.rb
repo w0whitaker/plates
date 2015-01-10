2bf327dd121280b4cbbe1cc2a2d03c8366d3481f
brazilla-ray <william@mrwilliamwhitaker.com>
2015-01-10 12:00:43 -0500

    add site logo to all pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,21 +8,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
  	assert_select "div.logo"
  end
 
- test "site logo on home page" do
- 	get root_path
- 	assert_template 'static_pages/home'
+ test "site logo on contact page" do
+ 	get contact_path
+ 	assert_template 'static_pages/contact'
  	assert_select "div.logo"
  end
  
- test "site logo on home page" do
- 	get root_path
- 	assert_template 'static_pages/home'
+ test "site logo on cv page" do
+ 	get cv_path
+ 	assert_template 'static_pages/cv'
  	assert_select "div.logo"
  end
 
- test "site logo on home page" do
- 	get root_path
- 	assert_template 'static_pages/home'
+ test "site logo on images page" do
+ 	get images_path
+ 	assert_template 'static_pages/images'
  	assert_select "div.logo"
  end
 
