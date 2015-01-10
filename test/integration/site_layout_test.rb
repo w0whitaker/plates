15b453b4aacb0a666ec8e08b388df1d7bb18abae
brazilla-ray <william@mrwilliamwhitaker.com>
2015-01-10 16:18:30 -0500

    finish putting footer on all pages(except maint.)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,28 +4,36 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
  	get root_path
  	assert_template 'static_pages/home'
  	assert_select "a[href=?]", root_path
- 	assert_select "a[href=?]", contact_path
- 	assert_select "a[href=?]", cv_path
  	assert_select "a[href=?]", images_path
+ 	assert_select "a[href=?]", cv_path
+ 	assert_select "a[href=?]", contact_path
  end
  test "links on contact page" do
  	get contact_path
  	assert_template 'static_pages/contact'
  	assert_select "a[href=?]", root_path
+ 	assert_select "a[href=?]", images_path
+ 	assert_select "a[href=?]", cv_path
+ 	assert_select "a[href=?]", contact_path
  end
  test "links on cv page" do
  	get cv_path
  	assert_template 'static_pages/cv'
  	assert_select "a[href=?]", root_path
+ 	assert_select "a[href=?]", images_path
+ 	assert_select "a[href=?]", cv_path
+ 	assert_select "a[href=?]", contact_path
  end
  test "links on images page" do
  	get images_path
  	assert_template 'static_pages/images'
  	assert_select "a[href=?]", root_path
+ 	assert_select "a[href=?]", images_path
+ 	assert_select "a[href=?]", cv_path
+ 	assert_select "a[href=?]", contact_path
  end
  test "links on maintenance page" do
  	get maintenance_path
  	assert_template 'static_pages/maintenance'
- 	assert_select "a[href=?]", root_path
  end
 end
