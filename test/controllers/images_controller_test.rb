afa11de2f1a11676270fc2662597ccea2265e9bc
brazilla-ray <william@mrwilliamwhitaker.com>
2015-04-28 14:00:53 -0400

    bring tests up to current state, more or less

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @base_title = "William Whitaker"
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select "title", "images | #{@base_title}"
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_select "title", "new | #{@base_title}"
+  end
 end
