1e947ef3bec2a1405e0a39b67191f9e6f635e9ba
brazilla-ray <william@mrwilliamwhitaker.com>
2015-05-03 13:42:34 -0400

    fixed forms and paths to use the admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :images, only: [:index]
 
   scope '/admin' do
-    resources :images, only: [:new, :edit, :show, :destroy, :update]
+    resources :images, only: [:new, :create, :edit, :show, :destroy, :update]
     resources :users, only: [:show]
   end
   
